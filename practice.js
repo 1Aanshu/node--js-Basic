11b32405a60a2d74a6446235aa8d58c31eb22e02
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-17 09:29:57 +0545

    practice function

---
+++ practice.js
@@ -0,0 +1,13 @@
+// Write a function to add 2 numbers
+
+const sum = (a, b) => a + b;
+
+const result = sum(4, 3);
+
+console.log(result);
+
+console.log(sum(5, 6));
+// Q2. Write a js function to that checks username and password.
+// use bootstrap for login page design
+// set username and password as your email
+// Result alert you have logged in
