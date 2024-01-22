8e0f4baf74377af9b56a988f9d14ae380c048510
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-22 22:56:17 +0545

    nodemailer test

--- password/nodemailer.js
+++ password/nodemailer.js
@@ -1,6 +1,31 @@
 // Go to nodemailer.com
 // npm i nodemailer
 // find the email sending code
-// SMTP milaunu paryo mwith google
+// SMTP milaunu paryo with google
 // username and password (app password for gmail)
 // run the code
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "aanshu2dwiwedi@gmail.com",
+    pass: "hggx dysy qnur xcwb",
+  },
+});
+
+const mailOptions = {
+  from: "aanshu2dwiwedi@gmail.com",
+  to: "dwiwediukmsag@gmail.com",
+  subject: "Nodemailer",
+  text: "Learn about Nodemailer!",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
