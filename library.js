2fd0fcb57e0446907bfed3c22094d71faab09d5d
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-23 08:31:50 +0545

    Array check

--- library.js
+++ library.js
@@ -7,3 +7,11 @@ const divide = (a, b) => a / b;
 const sub = (a, b) => a - b;
 
 module.exports = { sum, mul, divide, sub };
+
+const foodA = ["Noodle", "Pasta", "Ice-cream"];
+const foodB = ["Fries", "Ice-cream", "Pasta", "Pizza"];
+
+const foodFinder = (foodA, foodB) =>
+  foodA.filter((item) => foodB.includes(item));
+result = foodFinder(foodA, foodB);
+console.log({ result });
