d9f618d04c7d8a7f78a9ebf7d5399d5fc80f648b
Aanshu Dwiwedi <aanshu2dwiwedi@gmail.com>
2024-01-25 00:39:35 +0545

    currency converter

--- password/curconverter.js
+++ password/curconverter.js
@@ -1,15 +1,11 @@
 const CC = require("currency-converter-lt");
 
-
-const newCC = () => {
-  currencyConverter
-    .from("USD")
-    .to("GBP")
-    .amount(125)
-    .convert()
-    .then((response) => {
-      console.log(response);
-    });
-};
-
-currencyConverter = newCC();
+let currencyConverter = new CC({});
+currencyConverter
+  .from("USD")
+  .to("GBP")
+  .amount(125)
+  .convert()
+  .then((response) => {
+    console.log(response);
+  });
